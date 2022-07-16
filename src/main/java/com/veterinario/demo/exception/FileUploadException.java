0157a6a775ca33c0f23bae1530ba298abdafbdf3
MatyFretes <maty.fretes10@gmail.com>
2022-07-15 23:31:40 -0300

    Galeria Backend

---
+++ src/main/java/com/veterinario/demo/exception/FileUploadException.java
@@ -0,0 +1,20 @@
+package com.veterinario.demo.exception;
+
+import com.veterinario.demo.message.FileMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class FileUploadException {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<FileMessage> maxSizeException(MaxUploadSizeExceededException exc){
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                .body(new FileMessage("Uno o mas archivos exceden el tamaño maximo"));
+    }
+
+}
+
