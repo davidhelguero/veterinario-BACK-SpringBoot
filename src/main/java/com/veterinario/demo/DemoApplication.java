ee58d4cb1c803f8e855ac530d5f6c10d2b96c967
David Helguero <dahelguero@gmail.com>
2022-07-21 20:12:51 -0300

    Modificación DemoApplication

--- src/main/java/com/veterinario/demo/DemoApplication.java
+++ src/main/java/com/veterinario/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.veterinario.demo;
 import com.veterinario.demo.service.FileService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.annotation.Resource;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DemoApplication  implements CommandLineRunner {
 
 	@Resource
